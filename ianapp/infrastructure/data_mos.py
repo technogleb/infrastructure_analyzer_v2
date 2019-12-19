87fb7693a3dcc501601fa385d0b8c48a5949fb1b
DdNn708 <genericsup@gmail.com>
2019-12-19 17:13:51 +0300

    Функция get_dataset_size переименована в define_order_in_download_queue
    Функция define_order_in_download_queue дополнена:
    * сортирует словарь DATASETS по значению досупному по ключу 'items_count'
    * сохраняет в словарь DATASETS ключ 'order_in_download_queue' отсортированный список.
    Порядок в списке от меньшего к большему используется при скачивании наборов данных

--- ianapp/infrastructure/data_mos.py
+++ ianapp/infrastructure/data_mos.py
@@ -3,12 +3,16 @@ import requests
 from ianapp.config import DATASETS, DATA_MOS_API_KEY, USERAGENT_COOKIE
 
 
-def get_dataset_size():
-    """ Дополняет справочник DATASETS дополнительной информацией о размере датасета.
-    Информацию о размере датасета получает из api (источник https://apidata.mos.ru/Docs#datasetInfo).
+def define_order_in_download_queue():
+    """ Определяет в какой последовательности скачивать наборы данных.
 
-    В каждый вложенный словарь в DATASETS создает ключ 'items_count' в который записывает
-    значение полученное по ключу 'ItemsCount' из результата запроса.
+    В каждый вложенный словарь в словаре DATASETS создает ключ 'items_count' в который записывает
+    значение полученное по ключу 'ItemsCount' из результата api запроса.
+    (api https://apidata.mos.ru/Docs#datasetInfo).
+
+    В корне словаря DATASETS создает ключ 'order_in_download_queue',
+    куда записывает список 'id' порядок которых опрееделяет значение доступное по ключу 'items_count'.
+    Порядок в списке от меньшего к большему.
     """
 
     # Копирует ключи справочника DATASETS в виде списка
@@ -25,7 +29,13 @@ def get_dataset_size():
         tmp = requests.get(url, params=params, headers=headers).json()
         DATASETS[set_id]['items_count'] = tmp['ItemsCount']
 
+    # Сортировка (от меньшего к большему) вложенных словарей по количеству записей 'items_count' в каждом наборе
+    tmp = sorted(DATASETS.items(), key=lambda x: x[1]['items_count'])
+    # Добавление в справочник ключа 'order_in_download_queue', где
+    # хранится последовательность для скачивания данных
+    DATASETS['order_in_download_queue'] = list(map(lambda x: x[1]['id'], tmp))
+
 
 if __name__ == '__main__':
-    get_dataset_size()
+    define_order_in_download_queue()
     print(DATASETS)
